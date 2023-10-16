dbead9610425ee73a20d361c18b269062e9f0648
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-10-16 15:45:53 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "My NixOS infrastructure";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, ... }@inputs:
+    with inputs;
+    let
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
+    in
+    {
+
+      formatter = forAllSystems
+        (system: nixpkgsFor.${system}.nixpkgs-fmt);
+
+      # Each subdirectory in ./machines is a host. Add them all to
+      # nixosConfiguratons. Host configurations need a file called
+      # configuration.nix that will be read first
+      nixosConfigurations = builtins.listToAttrs (map
+        (x: {
+          name = x;
+          value = nixpkgs.lib.nixosSystem {
+
+            # Make inputs and the flake itself accessible as module parameters.
+            # Technically, adding the inputs is redundant as they can be also
+            # accessed with flake-self.inputs.X, but adding them individually
+            # allows to only pass what is needed to each module.
+            specialArgs = { flake-self = self; } // inputs;
+
+            modules = [
+              (import "${./.}/machines/${x}/configuration.nix" { inherit self; })
+            ];
+
+          };
+        })
+        (builtins.attrNames (builtins.readDir ./machines)));
+
+    };
+}
---
+++ modules/nix-common/default.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, lib, flake-self, ... }:
+with lib;
+let cfg = config.paul.nix-common;
+in
+{
+
+  options.paul.nix-common = {
+    enable = mkEnableOption "activate nix-common";
+    disable-cache = mkEnableOption "not use binary-cache";
+  };
+
+  config = mkIf cfg.enable {
+
+    # Set the $NIX_PATH entry for nixpkgs. This is necessary in
+    # this setup with flakes, otherwise commands like `nix-shell
+    # -p pkgs.htop` will keep using an old version of nixpkgs.
+    # With this entry in $NIX_PATH it is possible (and
+    # recommended) to remove the `nixos` channel for both users
+    # and root e.g. `nix-channel --remove nixos`. `nix-channel
+    # --list` should be empty for all users afterwards
+    nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+    nixpkgs.overlays = [  ];
+
+    # Allow unfree licenced packages
+    nixpkgs.config.allowUnfree = true;
+
+    nix = {
+
+      package = pkgs.nixFlakes;
+      extraOptions = ''
+        # this enables the technically experimental feature Flakes
+        experimental-features = nix-command flakes
+
+        # If set to true, Nix will fall back to building from source if a binary substitute fails.
+        fallback = true
+
+        # the timeout (in seconds) for establishing connections in the binary cache substituter. 
+        connect-timeout = 10
+
+        # these log lines are only shown on a failed build
+        log-lines = 25
+
+        # Free up to 1GiB whenever there is less than 100MiB left.
+        min-free = ${toString (100 * 1024 * 1024)}
+        max-free = ${toString (1024 * 1024 * 1024)}
+      '';
+
+      settings = {
+        trusted-public-keys = mkIf (cfg.disable-cache != true) [
+          "nix-cache:4FILs79Adxn/798F8qk2PC1U8HaTlaPqptwNJrXNA1g="
+        ];
+        substituters = mkIf (cfg.disable-cache != true) [
+          "https://cache.lounge.rocks/nix-cache"
+        ];
+
+        # Users allowed to run nix
+        allowed-users = [ "root" ];
+
+        # Save space by hardlinking store files
+        auto-optimise-store = true;
+      };
+
+      # Clean up old generations after 30 days
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+
+    };
+
+  };
+}
