8bb5daa1365b4cbc23a7939c401a3bdce9e7b5aa
MayNiklas <info@niklas-steffen.de>
2023-10-24 09:28:42 +0200

    move packages to desktop

--- home-manager/profiles/common.nix
+++ home-manager/profiles/common.nix
@@ -1,10 +1,5 @@
 { config, pkgs, lib, flake-self, ... }:
 with lib;
-let
-  my-package = (pkgs.writeShellScriptBin "my-package" ''
-    echo "I'm a function representing a package!"
-  '');
-in
 {
   config = {
 
@@ -25,53 +20,13 @@ in
     # Install these packages for my user
     home.packages = with pkgs; [
       asciinema
-      bun
-      discord
       dnsutils
-      firefox
       glances
-      gparted
-      jellyfin-mpv-shim
-      lua
-      # gnomeExtensions.galaxy-buds-battery
-      # gnomeExtensions.battery-health-charging
-      mpv
       nil
       nix-top
       nixpkgs-fmt
-      nvtop
-      obsidian
-      # p3x-onenote
-      protonmail-bridge
-      prusa-slicer
-      ripgrep
-      signal-desktop
-      stylua
-      thunderbird-bin
       unzip
       usbutils
-      xournalpp
-      zoom-us
-      gnomeExtensions.gesture-improvements
-      gnomeExtensions.just-perfection
-      gnomeExtensions.blur-my-shell
-      gnomeExtensions.burn-my-windows
-      # My packages
-
-      # example for a function representing a package
-      my-package
-
-      # example for a function building python with some packages
-      (python3.withPackages (ps: with ps; [
-        requests
-        numpy
-      ]))
-
-      # example for a function bulding a package from a shell script
-      (writeShellScriptBin "bin-package" ''
-        ${hello}/bin/hello
-        echo "We can use packages from the nixpkgs collection!"
-      '')
     ];
 
     # Let Home Manager install and manage itself.
--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -1,4 +1,9 @@
 { pkgs, lib, config, ... }:
+let
+  my-package = (pkgs.writeShellScriptBin "my-package" ''
+    echo "I'm a function representing a package!"
+  '');
+in
 with lib;
 {
   config = {
@@ -8,7 +13,46 @@ with lib;
     };
 
     # Install these packages for my user
-    home.packages = with pkgs; [ ];
+    home.packages = with pkgs; [
+      bun
+      discord
+      firefox
+      gparted
+      jellyfin-mpv-shim
+      lua
+      # gnomeExtensions.galaxy-buds-battery
+      # gnomeExtensions.battery-health-charging
+      mpv
+      nvtop
+      obsidian
+      # p3x-onenote
+      protonmail-bridge
+      prusa-slicer
+      ripgrep
+      signal-desktop
+      stylua
+      thunderbird-bin
+      xournalpp
+      zoom-us
+      gnomeExtensions.gesture-improvements
+
+      # My packages
+
+      # example for a function representing a package
+      my-package
+
+      # example for a function building python with some packages
+      (python3.withPackages (ps: with ps; [
+        requests
+        numpy
+      ]))
+
+      # example for a function bulding a package from a shell script
+      (writeShellScriptBin "bin-package" ''
+        ${hello}/bin/hello
+        echo "We can use packages from the nixpkgs collection!"
+      '')
+    ];
 
   };
 }
