e23100aee13779e8c04601036ba46e219ae6db06
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-10-26 15:58:52 +0200

    reenable gdm, it has decided to fix itself

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -14,8 +14,8 @@ in
     services.xserver.enable = true;
 
     # Enable the GNOME Desktop Environment.
-    # services.xserver.displayManager.gdm.enable = true;
-    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    # services.xserver.displayManager.sddm.enable = true;
 
     services.xserver.desktopManager.gnome.enable = true;
 
