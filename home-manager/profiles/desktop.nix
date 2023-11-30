955b09bb897fddd5c9b6349dfc58632d2a4077d2
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-11-30 15:43:18 +0100

    add gnomeExtension clipboard-history

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -38,6 +38,7 @@ with lib;
       gnomeExtensions.gesture-improvements
       gnomeExtensions.just-perfection
       gnomeExtensions.gsconnect
+      gnomeExtensions.clipboard-history
     ];
   };
 }
