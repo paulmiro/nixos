5cc8bdaff18f5e6f1b880fee10d02400e6668fcb
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-12-07 02:19:21 +0100

    add machine hawking

---
+++ machines/hawking/configuration.nix
@@ -0,0 +1,32 @@
+{ self, ... }:
+{ config, pkgs, ... }:
+
+{
+
+  paul = {
+    common-server.enable = true;
+    systemd-boot.enable = true;
+  };
+
+  imports = [
+    # Include the results of the hardware scan.
+    # ./hardware-configuration.nix
+  ];
+
+  # enable all the firmware with a license allowing redistribution
+  hardware.enableRedistributableFirmware = true;
+
+  networking = {
+    networkmanager.enable = true;
+    hostName = "hawking";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # being able to build aarm64 stuff
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  environment.systemPackages = with pkgs; [ ];
+
+}
