9116a3751e337b7fc1ba7c043864277fd654f615
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-12-14 11:03:29 +0100

    jellyfin: add docker compose file (deactivated for now)

--- modules/jellyfin/default.nix
+++ modules/jellyfin/default.nix
@@ -11,6 +11,26 @@ in
 
   config = mkIf cfg.enable {
 
+/*
+    systemd.services.jellyfin =
+      let
+        compose-file = fileContents ./docker-compose.yml;
+      in
+      {
+        description = "a docker compose app.";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "docker.service" "docker.socket" ];
+        serviceConfig = mkMerge [
+          {
+            User = "root";
+            Group = "root";
+            ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f ${compose-file} up";
+            Restart = "on-failure";
+          }
+        ];
+        preStop = "${pkgs.docker-compose}/bin/docker-compose -f ${compose-file} down";
+      };
+*/
     services.nginx.virtualHosts."***REMOVED***" = mkIf cfg.enableNginx {
       enableACME = true;
       forceSSL = true;
