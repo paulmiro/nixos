0a34ba3c28ff10b2305fd1098f0a3450297ec89d
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-12-15 19:13:05 +0100

    add module flaresolverr

---
+++ modules/flaresolverr/default.nix
@@ -0,0 +1,36 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.paul.flaresolverr;
+in
+{
+
+  options.paul.flaresolverr = {
+    enable = mkEnableOption "activate flaresolverr";
+    openFirewall = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Documentation placeholder
+      '';
+    };
+
+    port = mkOption {
+      type = types.str;
+      default = "8191";
+      description = ''
+        Documentation placeholder
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+
+    virtualisation.oci-containers.containers.flaresolverr = {
+      autoStart = true;
+      image = "flaresolverr/flaresolverr:latest";
+      ports = [ "8191:${cfg.port}/tcp" ];
+    };
+
+  };
+}
