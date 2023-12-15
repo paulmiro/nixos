a509808e23210b64e977db32f5f66c8e5b69553e
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-12-15 19:13:57 +0100

    add module prowlarr

---
+++ modules/prowlarr/default.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.paul.prowlarr;
+in
+{
+
+  options.paul.prowlarr = {
+    enable = mkEnableOption "activate prowlarr";
+    openFirewall = mkOption {
+      type = types.bool;
+      default = true;
+      description = "allow prowlarr port in firewall";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    paul.flaresolverr.enable = true;
+
+    services.prowlarr = {
+      enable = true;
+      openFirewall = cfg.openFirewall;
+    };
+  };
+}
