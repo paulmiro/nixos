eb55693e9ba87f5d477908ac84457065caf4be4b
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-12-15 15:58:00 +0100

    add config for sonarr and arr group

---
+++ modules/sonarr/default.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.paul.sonarr;
+in
+{
+
+  options.paul.sonarr = {
+    enable = mkEnableOption "activate sonarr";
+    openFirewall = mkEnableOption "allow sonarr port in firewall";
+  };
+
+  config = mkIf cfg.enable {
+    paul.group.arr.enable = true;
+    services.sonarr = {
+      enable = true;
+      user = "sonarr";
+      group = "arr";
+      dataDir = "/var/lib/sonarr/.config/NzbDrone";
+      openFirewall = cfg.openFirewall;
+    };
+  };
+}
