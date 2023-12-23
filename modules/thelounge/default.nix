ab4c6141567b5377232af19af78280265e3caf7a
paulmiro <30203227+paulmiro@users.noreply.github.com>
2023-12-23 02:34:40 +0100

    add thelounge

---
+++ modules/thelounge/default.nix
@@ -0,0 +1,31 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.paul.thelounge;
+in
+{
+
+  options.paul.thelounge = {
+    enable = mkEnableOption "activate thelounge";
+    openFirewall = mkEnableOption "allow thelounge port in firewall";
+    port = mkOption {
+      type = types.port;
+      default = 9000;
+      description = "port to listen on";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    services.thelounge = {
+      enable = true;
+      port = cfg.port;
+      plugins = [ pkgs.theLoungePlugins.themes.flat-dark ];
+      extraConfig = {
+        theme = "thelounge-theme-flat-dark";
+
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
+  };
+}
