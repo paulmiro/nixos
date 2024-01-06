4a488a5fa0c78e2a6bb6860792ab5b8af855e530
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-01-06 19:18:57 +0100

    add immich (doesn't work yet)

--- modules/nfs-mounts/default.nix
+++ modules/nfs-mounts/default.nix
@@ -6,7 +6,9 @@ in
 
   options.paul.nfs-mounts = {
     enableData = mkEnableOption "activate TANK1/data";
+    enablePhotos = mkEnableOption "activate TANK1/photos";
     enableJellyfin = mkEnableOption "activate BLITZ1/jellyfin";
+    enableImmich = mkEnableOption "activate BLITZ1/immich";
     enablePlayground = mkEnableOption "activate TANK1/playground";
   };
 
@@ -15,8 +17,16 @@ in
       device = "turing:/mnt/TANK1/data";
       fsType = "nfs";
     };
+    fileSystems."/mnt/nfs/photos" = mkIf cfg.enablePhotos {
+      device = "turing:/mnt/TANK1/photos";
+      fsType = "nfs";
+    };
     fileSystems."/mnt/nfs/jellyfin" = mkIf cfg.enableJellyfin {
       device = "turing:/mnt/BLITZ1/jellyfin";
+      fsType = "nfs";
+    };
+    fileSystems."/mnt/nfs/immich" = mkIf cfg.enableImmich {
+      device = "turing:/mnt/BLITZ1/immich";
       fsType = "nfs";
     };
     fileSystems."/mnt/nfs/playground" = mkIf cfg.enablePlayground {
