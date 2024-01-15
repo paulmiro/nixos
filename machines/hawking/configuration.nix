1949b6a63d15c83355ac7e265850f93b8e816857
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-01-15 12:53:30 +0100

    add nextcloud nginx

--- machines/hawking/configuration.nix
+++ machines/hawking/configuration.nix
@@ -108,13 +108,22 @@
       return = "302 https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     };
   };
-  paul.dyndns.domains = [ "***REMOVED***" "***REMOVED***" ];
+  paul.dyndns.domains = [ "***REMOVED***" "***REMOVED***" "***REMOVED***" ];
 
   services.nginx.virtualHosts."***REMOVED***" = {
     enableACME = true;
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://192.168.178.222:30044";
+    };
+    enableGeoBlocking = true;
+  };
+
+  services.nginx.virtualHosts."***REMOVED***" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://192.168.178.222:9001";
     };
     enableGeoBlocking = true;
   };
