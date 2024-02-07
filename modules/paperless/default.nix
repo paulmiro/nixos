9892ac2dfd5e93b208761420e2ae37fb32e06e99
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-02-07 19:51:14 +0100

    add paperless (not enabled yet, does not seem to work yet)

---
+++ modules/paperless/default.nix
@@ -0,0 +1,29 @@
+{ lib, config, ... }:
+with lib;
+let cfg = config.paul.paperless;
+in
+{
+
+  options.paul.paperless = {
+    enable = mkEnableOption "activate paperless";
+    openFirewall = mkOption {
+      type = types.bool;
+      default = true;
+      description = "allow paperless port in firewall";
+    };
+    port = mkOption {
+      type = types.port;
+      default = 28981;
+      description = "port to run paperless on";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.paperless = {
+      enable = true;
+      port = cfg.port;
+      address = "hawking";
+    };
+    networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
+  };
+}
