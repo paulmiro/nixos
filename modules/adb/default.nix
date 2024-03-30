bae79bac6d40cb439b7a27ddd0665b83ce88313b
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-03-30 15:44:46 +0100

    add adb module

---
+++ modules/adb/default.nix
@@ -0,0 +1,16 @@
+{ lib, config, ... }:
+with lib;
+let cfg = config.paul.adb;
+in
+{
+  options.paul.adb = {
+    enable = mkEnableOption "Enable Android development tools";
+  };
+
+  config = mkIf cfg.enable {
+    programs.adb.enable = true;
+    users.users.paulmiro.extraGroups = [ "adbusers" ];
+    networking.firewall.allowedTCPPorts = [ 8081 ];
+    networking.firewall.allowedUDPPorts = [ 8081 ];
+  };
+}
