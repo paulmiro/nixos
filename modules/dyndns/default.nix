30bf061b4a88f2757d9b6aaf886c7d1e89e09b06
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-04-13 15:00:59 +0200

    use bitwarden for cloudflare api token

--- modules/dyndns/default.nix
+++ modules/dyndns/default.nix
@@ -11,15 +11,27 @@ in
       default = [ ];
       description = "domains to update";
     };
+
+    apiTokenFile = mkOption {
+      type = types.str;
+      default = "/run/keys/cloudflare-api-token";
+      description = "path to the file containing the cloudflare api token";
+    };
   };
 
   config = mkIf cfg.enable {
     services.cloudflare-dyndns = {
       enable = true;
-      apiTokenFile = "${"${self}/secrets/cloudflare-token"}";
+      apiTokenFile = cfg.apiTokenFile;
       ipv4 = true;
       ipv6 = false;
       domains = cfg.domains;
+    };
+
+    lollypops.secrets.files."cloudflare-api-token" = {
+      cmd = "echo \"CLOUDFLARE_API_TOKEN=$(bw get item cloudflare | jq -r '.fields[] | select(.name == \"api-token-edit-dns-all\") | .value')\"";
+      path = cfg.apiTokenFile;
+      mode = "0444";
     };
   };
 }
