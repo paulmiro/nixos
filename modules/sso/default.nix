93cc659f30101e52149f117d64e15e4a8ebc65dd
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-04-30 16:31:01 +0200

    add sso module and activate it

---
+++ modules/sso/default.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.paul.sso;
+in
+{
+
+  options.paul.sso = {
+    enable = mkEnableOption "activate sso-stack";
+
+    keycloak = {
+      openFirewall = mkEnableOption "allow keycloak port in firewall";
+      enableNginx = mkEnableOption "activate nginx proxy";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    paul.keycloak.enable = true;
+  };
+
+}
+
+
