6a2540a5d7b1db232cc62849372d1fca9598bb4b
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-05-03 19:37:22 +0200

    remove bitwarden-cli

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -12,7 +12,6 @@ with lib;
 
     # Install these packages for my user
     home.packages = with pkgs; [
-      bitwarden-cli # TODO: remove
       burpsuite
       ungoogled-chromium
       discord
