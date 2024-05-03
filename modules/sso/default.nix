adf73022bb211f6e17b7b000cbfae6bd0171ff42
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-05-03 17:52:16 +0200

    add oauth2_proxy module

--- modules/sso/default.nix
+++ modules/sso/default.nix
@@ -7,9 +7,30 @@ in
   options.paul.sso = {
     enable = mkEnableOption "activate sso-stack";
 
+    baseDomain = mkOption {
+      type = types.str;
+      default = "***REMOVED***";
+      description = "The base domain for the sso stack";
+    };
+
+    subDomain = mkOption {
+      type = types.str;
+      default = "auth";
+      description = "The subdomain for the sso stack";
+    };
+
     keycloak = {
       openFirewall = mkEnableOption "allow keycloak port in firewall";
       enableNginx = mkEnableOption "activate nginx proxy";
+    };
+
+    # TODO: Does this option even make sense?
+    oauth2_proxy = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = "activate oauth2_proxy";
+      };
     };
   };
 
@@ -18,6 +39,13 @@ in
       enable = true;
       openFirewall = cfg.keycloak.openFirewall;
       enableNginx = cfg.keycloak.enableNginx;
+      domain = "${cfg.subDomain}.${cfg.baseDomain}";
+    };
+
+    paul.oauth2_proxy = {
+      enable = cfg.oauth2_proxy.enable;
+      domain = "${cfg.subDomain}.${cfg.baseDomain}";
+      baseDomain = cfg.baseDomain;
     };
 
   };
