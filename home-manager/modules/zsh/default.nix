4c7d7634121d528add78245aa93152958b003608
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-05-03 19:37:15 +0200

    edit use rbw for secret management instead of bitwarden-cli

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -73,7 +73,7 @@ let cfg = config.paul.programs.zsh; in
         frb = "${pkgs.nixos-rebuild}/bin/nixos-rebuild --use-remote-sudo switch --flake";
 
         # switch another machine from within a flake repository
-        lolly = "bwu; ${pkgs.nix}/bin/nix run .\#lollypops --";
+        lolly = "${pkgs.nix}/bin/nix run .\#lollypops --";
 
         # always execute nixos-rebuild with sudo for switching
         nixos-rebuild = "${pkgs.nixos-rebuild}/bin/nixos-rebuild --use-remote-sudo";
@@ -84,13 +84,8 @@ let cfg = config.paul.programs.zsh; in
         # Other
         lsblk = "${pkgs.util-linux}/bin/lsblk -o name,mountpoint,label,size,type,uuid";
 
-        # bitwarden-cli
-        bwu = "if [[ \"$(${pkgs.bitwarden-cli}/bin/bw unlock --check)\" != 'Vault is unlocked!' ]]; then export BW_SESSION=$(${pkgs.bitwarden-cli}/bin/bw unlock --raw); fi";
-
-        bwl = "${pkgs.bitwarden-cli}/bin/bw lock";
-
         # fun stuff
-        
+
         # mensa
         mensa = "${pkgs.nix}/bin/nix run 'github:alexanderwallau/bonn-mensa' --";
 
