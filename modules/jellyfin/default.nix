17b6226a4bad6f6cd6c56d29aff5a51da490fc12
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-06-03 11:00:26 +0200

    update jellyfin to 10.9.4

--- modules/jellyfin/default.nix
+++ modules/jellyfin/default.nix
@@ -55,7 +55,7 @@ in
 
       virtualisation.oci-containers.backend = "docker";
       virtualisation.oci-containers.containers.jellyfin = {
-        image = "jellyfin/jellyfin:10.9.3";
+        image = "jellyfin/jellyfin:10.9.4";
         user = "4001:4001";
         volumes = [
           "/mnt/nfs/jellyfin/config:/config"
