2de3919959506663c60bda65c80e1ee675f31090
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-07-23 21:09:34 +0200

    remove deprecated sound option

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -11,7 +11,6 @@ in
   config = mkIf cfg.enable {
 
     # Enable sound with pipewire.
-    sound.enable = true;
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
