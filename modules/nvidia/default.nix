be503b3e9fde0760c85258275700d46ce5ecb759
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-08-09 10:59:47 +0200

    chore: virtualisation.docker.enableNvidia is now hardware.nvidia-container-toolkit.enable

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -24,8 +24,6 @@ in
 
     environment.systemPackages = with pkgs; [ nvtopPackages.full ];
 
-    virtualisation.docker.enableNvidia = mkIf config.virtualisation.docker.enable true;
-
     hardware = {
 
       # Enable OpenGL
@@ -64,6 +62,8 @@ in
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
         package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
+
+      nvidia-container-toolkit.enable = mkIf config.virtualisation.docker.enable true;
 
     };
 
