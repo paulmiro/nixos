0864d3880800c66834245a2abfae377b7fe07662
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-11-11 19:04:31 +0100

    add nfs-mounts dependency on tailscale

--- modules/nfs-mounts/default.nix
+++ modules/nfs-mounts/default.nix
@@ -18,30 +18,37 @@ in
     fileSystems."/mnt/nfs/arr" = mkIf cfg.enableArr {
       device = "turing:/mnt/TANK2/arr";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
     fileSystems."/mnt/nfs/photos" = mkIf cfg.enablePhotos {
       device = "turing:/mnt/TANK2/photos";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
     fileSystems."/mnt/nfs/jellyfin" = mkIf cfg.enableJellyfin {
       device = "turing:/mnt/BLITZ1/apps/jellyfin";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
     fileSystems."/mnt/nfs/immich" = mkIf cfg.enableImmich {
       device = "turing:/mnt/BLITZ1/apps/immich";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
     fileSystems."/mnt/nfs/authentik" = mkIf cfg.enableAuthentik {
       device = "turing:/mnt/BLITZ1/apps/authentik";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
     fileSystems."/mnt/nfs/playground" = mkIf cfg.enablePlayground {
       device = "turing:/mnt/BLITZ1/playground";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
     fileSystems."/mnt/nfs/postgres_backup" = mkIf cfg.enablePostgresBackup {
       device = "turing:/mnt/TANK2/backups/postgres";
       fsType = "nfs";
+      options = [ "x-systemd.requires=tailscaled.service" ];
     };
   };
 
