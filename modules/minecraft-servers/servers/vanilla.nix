ed85c7355fd63927d80d52ef4b727d2fdf78cc22
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-02-17 23:48:55 +0100

    add BlueMap to vanilla minecraft

--- modules/minecraft-servers/servers/vanilla.nix
+++ modules/minecraft-servers/servers/vanilla.nix
@@ -32,6 +32,16 @@ let cfg = config.paul.minecraft-servers.vanilla; in
           openFirewall = true;
           autoStart = true;
 
+          symlinks = {
+            plugins = pkgs.linkFarmFromDrvs "plugins"
+              (builtins.attrValues {
+                BlueMap = fetchurl {
+                  url = "https://cdn.modrinth.com/data/swbUV1cr/versions/KDFOHrSO/bluemap-5.5-paper.jar";
+                  sha512 = "858805ca7187216b82817fb3e697a9d5bfb8d215f399dee653f9152bea4b6292d1d271c6287195cafa53cdec774e964e8a6d6a7ed018e397e72525d102c4dc0c";
+                };
+              });
+          };
+
           serverProperties = {
             enforce-secure-profile = false; # turns off message signing
             enforce-whitelist = true;
