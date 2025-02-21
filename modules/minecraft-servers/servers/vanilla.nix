92005d9a3b4f83f4782b47a28b7bdf8929440396
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-02-21 16:08:03 +0100

    add bluemap to vanilla minecraft server (for real now)

--- modules/minecraft-servers/servers/vanilla.nix
+++ modules/minecraft-servers/servers/vanilla.nix
@@ -19,6 +19,16 @@ let cfg = config.paul.minecraft-servers.vanilla; in
       enable = true;
       domains = [ cfg.domain ];
     };
+
+    services.nginx.virtualHosts."${cfg.domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8100";
+        geo-ip = true;
+      };
+    };
+
     services.minecraft-servers = {
       enable = true;
       eula = true;
@@ -30,6 +40,13 @@ let cfg = config.paul.minecraft-servers.vanilla; in
           package = pkgs.paperServers.paper-1_21_4;
           openFirewall = true;
           autoStart = true;
+
+          symlinks = {
+            "plugins/bluemap-5.5-paper.jar" = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/swbUV1cr/versions/KDFOHrSO/bluemap-5.5-paper.jar";
+              sha512 = "858805ca7187216b82817fb3e697a9d5bfb8d215f399dee653f9152bea4b6292d1d271c6287195cafa53cdec774e964e8a6d6a7ed018e397e72525d102c4dc0c";
+            };
+          };
         };
       };
     };
