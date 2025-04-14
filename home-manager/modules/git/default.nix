728fc48507b7d41fba4a215f3311333287aba8fc
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-04-14 14:29:12 +0200

    remove default gitignores

--- home-manager/modules/git/default.nix
+++ home-manager/modules/git/default.nix
@@ -13,42 +13,6 @@ in
         lfs.enable = true;
         ignores = [
           ".vscode/"
-          /*
-          "tags"
-          */
-          "*.swp"
-          # Nix builds
-          /*
-          "result"
-          */
-          # Core latex/pdflatex auxiliary files
-          /*
-          "*.aux"
-          "*.lof"
-          "*.log"
-          "*.lot"
-          "*.fls"
-          "*.out"
-          "*.toc"
-          "*.fmt"
-          "*.fot"
-          "*.cb"
-          "*.cb2"
-          */
-          ".*.lb"
-          # Python
-          "__pycache__/"
-          "*.py[cod]"
-          "*$py.class"
-          ".Python"
-          /*
-          "build/"
-          "develop-eggs/"
-          "dist/"
-          */
-          # Web
-          "node_modules/"
-          "bun.lockb"
         ];
         extraConfig = {
           pull.rebase = false;
