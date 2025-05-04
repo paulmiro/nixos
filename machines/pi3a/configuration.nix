f70ff5da7774cd8234cd35483e78d7a2c8143694
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-05-04 16:35:10 +0200

    add pi3a

---
+++ machines/pi3a/configuration.nix
@@ -0,0 +1,44 @@
+# to remotely deploy while building on your system:
+# nixos-rebuild switch --flake '.#pi3a' --target-host 'root@192.182.X.X' -L 
+# to build the SD image:
+# nix build .\#nixosConfigurations.pi3a.config.system.build.sdImage
+{ self, ... }:
+{ pkgs, lib, config, modulesPath, flake-self, home-manager, nixos-hardware, nixpkgs, ... }: {
+
+  paul = {
+    common-server.enable = true;
+  };
+
+  imports = [
+    # being able to build the sd-image
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+
+    # https://github.com/NixOS/nixos-hardware/tree/master/raspberry-pi/4
+    nixos-hardware.nixosModules.raspberry-pi-3
+  ];
+
+  ### build sd-image
+
+  # nix build .\#nixosConfigurations.pi3a.config.system.build.sdImage
+  # add boot.binfmt.emulatedSystems = [ "aarch64-linux" ]; to your x86 system
+  # to build ARM stuff through qemu
+  sdImage.compressImage = false;
+  sdImage.imageBaseName = "raspi-image";
+
+
+
+  networking = {
+    hostName = "pi3a";
+    networkmanager.enable = true;
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
