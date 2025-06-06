0ad957eb958a66720c24107916d96deda278eb0a
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-06-06 16:12:12 +0200

    automount nfs mounts

--- modules/nfs-mounts/default.nix
+++ modules/nfs-mounts/default.nix
@@ -21,37 +21,58 @@ in
     fileSystems."/mnt/nfs/arr" = lib.mkIf cfg.enableArr {
       device = "turing:/mnt/TANK2/arr";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
     fileSystems."/mnt/nfs/photos" = lib.mkIf cfg.enablePhotos {
       device = "turing:/mnt/TANK2/photos";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
     fileSystems."/mnt/nfs/jellyfin" = lib.mkIf cfg.enableJellyfin {
       device = "turing:/mnt/BLITZ1/apps/jellyfin";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
     fileSystems."/mnt/nfs/immich" = lib.mkIf cfg.enableImmich {
       device = "turing:/mnt/BLITZ1/apps/immich";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
     fileSystems."/mnt/nfs/authentik" = lib.mkIf cfg.enableAuthentik {
       device = "turing:/mnt/BLITZ1/apps/authentik";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
     fileSystems."/mnt/nfs/playground" = lib.mkIf cfg.enablePlayground {
       device = "turing:/mnt/BLITZ1/playground";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
     fileSystems."/mnt/nfs/postgres_backup" = lib.mkIf cfg.enablePostgresBackup {
       device = "turing:/mnt/TANK2/backups/postgres";
       fsType = "nfs";
-      options = [ "x-systemd.requires=tailscaled.service" ];
+      options = [
+        "x-systemd.automount"
+        "noauto"
+      ];
     };
   };
 }
