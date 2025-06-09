117e6e49c5b0949356ff3d30086d453b76679883
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-06-09 21:16:48 +0200

    correct mutableUsers comment

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -28,7 +28,6 @@ in
     environment.systemPackages = with pkgs; [
     ];
 
-    # this is already the default and only here to throw an error if it is ever set to true anywhere else
     # to make sure clan vars can manage passwords correctly
     users.mutableUsers = false;
   };
