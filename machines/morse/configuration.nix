99a02fd07706493ec784d6d5f81f8e357595e6a1
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-06-20 15:15:49 +0200

    add github redirect for my domain

--- machines/morse/configuration.nix
+++ machines/morse/configuration.nix
@@ -59,6 +59,16 @@
     ];
   };
 
+  services.nginx.virtualHosts."paulmiro.de" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      return = "301 https://github.com/paulmiro";
+    };
+  };
+
+  paul.dyndns.domains = [ "paulmiro.de" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
