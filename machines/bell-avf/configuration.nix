525732fa88d84cf22a4080dca7e315d6cff342ce
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-06-25 20:49:25 +0200

    add tailscale on bell-avf

--- machines/bell-avf/configuration.nix
+++ machines/bell-avf/configuration.nix
@@ -21,6 +21,11 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
