3c7852f598b081d49db59bcd0330da487d73423c
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-07-07 16:25:32 +0200

    add nix caches: nix-community and clan

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -55,11 +55,15 @@ in
 
       settings = {
         trusted-public-keys = lib.mkIf (cfg.disable-cache != true) [
-          "nix-cache:4FILs79Adxn/798F8qk2PC1U8HaTlaPqptwNJrXNA1g="
+          "nix-cache:4FILs79Adxn/798F8qk2PC1U8HaTlaPqptwNJrXNA1g=" # lounge.rocks
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
         ];
 
         substituters = lib.mkIf (cfg.disable-cache != true) [
           "https://cache.lounge.rocks/nix-cache"
+          "https://cache.clan.lol"
+          "https://nix-community.cachix.org"
         ];
 
         trusted-users = [
