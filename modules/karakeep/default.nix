6e5eb414355a219dc760eed52317e72fb4e69724
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-07-15 00:30:21 +0200

    fix karakeep networking config

--- modules/karakeep/default.nix
+++ modules/karakeep/default.nix
@@ -26,57 +26,66 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.port ];
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        services.karakeep = {
+          enable = true;
+          extraEnvironment = {
+            PORT = toString cfg.port;
+            NEXTAUTH_URL = "https://${cfg.domain}";
+            DISABLE_NEW_RELEASE_CHECK = "true";
+            OAUTH_WELLKNOWN_URL = "https://${config.paul.private.domains.authentik}/application/o/karakeep/.well-known/openid-configuration";
+            OAUTH_PROVIDER_NAME = config.paul.private.authentik_display_name;
+            DISABLE_PASSWORD_AUTH = "true";
+          };
+          environmentFile = config.clan.core.vars.generators.karakeep.files.env.path;
+        };
 
-    services.nginx.virtualHosts."${cfg.domain}" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString cfg.port}";
-        geo-ip = true;
-      };
-    };
+        # required because of state-version shenanigans
+        # TODO: 25.11: remove this (assuming they actually removed meilisearch_1_11)
+        services.meilisearch.package = pkgs.meilisearch;
 
-    services.karakeep = {
-      enable = true;
-      extraEnvironment = {
-        PORT = toString cfg.port;
-        NEXTAUTH_URL = "https://${cfg.domain}";
-        DISABLE_NEW_RELEASE_CHECK = "true";
-        OAUTH_WELLKNOWN_URL = "https://${config.paul.private.domains.authentik}/application/o/karakeep/.well-known/openid-configuration";
-        OAUTH_PROVIDER_NAME = config.paul.private.authentik_display_name;
-        DISABLE_PASSWORD_AUTH = "true";
-      };
-      environmentFile = config.clan.core.vars.generators.karakeep.files.env.path;
-    };
+        clan.core.vars.generators.karakeep = {
+          prompts.oauth-client-id.description = "Karakeep OAuth2 Client ID";
+          prompts.oauth-client-id.type = "hidden";
+          prompts.oauth-client-id.persist = false;
 
-    # required because of state-version shenanigans
-    # TODO: 25.11: remove this (assuming they actually removed meilisearch_1_11)
-    services.meilisearch.package = pkgs.meilisearch;
+          prompts.oauth-client-secret.description = "Karakeep OAuth2 Client Secret";
+          prompts.oauth-client-secret.type = "hidden";
+          prompts.oauth-client-secret.persist = false;
 
-    clan.core.vars.generators.karakeep = {
-      prompts.oauth-client-id.description = "Karakeep OAuth2 Client ID";
-      prompts.oauth-client-id.type = "hidden";
-      prompts.oauth-client-id.persist = false;
+          prompts.openai-api-key.description = "Karakeep OpenAI API Key";
+          prompts.openai-api-key.type = "hidden";
+          prompts.openai-api-key.persist = false;
 
-      prompts.oauth-client-secret.description = "Karakeep OAuth2 Client Secret";
-      prompts.oauth-client-secret.type = "hidden";
-      prompts.oauth-client-secret.persist = false;
+          files.env.secret = true;
 
-      prompts.openai-api-key.description = "Karakeep OpenAI API Key";
-      prompts.openai-api-key.type = "hidden";
-      prompts.openai-api-key.persist = false;
+          script = ''
+            echo "\
+            OAUTH_CLIENT_ID="$(cat $prompts/oauth-client-id)"
+            OAUTH_CLIENT_SECRET="$(cat $prompts/oauth-client-secret)"
+            OPENAI_API_KEY="$(cat $prompts/openai-api-key)"
+            " > $out/env
+          '';
+        };
+      }
+      (lib.mkIf cfg.openFirewall {
+        networking.firewall.allowedTCPPorts = [ cfg.port ];
+      })
+      (lib.mkIf cfg.enableNginx {
+        paul.nginx.enable = true;
+        paul.dyndns.domains = [ cfg.domain ];
 
-      files.env.secret = true;
-
-      script = ''
-        echo "\
-        OAUTH_CLIENT_ID="$(cat $prompts/oauth-client-id)"
-        OAUTH_CLIENT_SECRET="$(cat $prompts/oauth-client-secret)"
-        OPENAI_API_KEY="$(cat $prompts/openai-api-key)"
-        " > $out/env
-      '';
-    };
-  };
+        services.nginx.virtualHosts."${cfg.domain}" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:${toString cfg.port}";
+            geo-ip = true;
+          };
+        };
+      })
+    ]
+  );
 }
