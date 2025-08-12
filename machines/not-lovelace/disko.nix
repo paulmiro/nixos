75d42537c9de409349db7a03f1c41e2285fcec25
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-08-12 22:34:49 +0200

    full disko config again

--- machines/not-lovelace/disko.nix
+++ machines/not-lovelace/disko.nix
@@ -1,33 +1,33 @@
 {
   disko.devices = {
     disk = {
-      # zroot1 = {
-      #   # Boot/Root SSD 1
-      #   type = "disk";
-      #   device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HBJQ-000_S50HNX0N611770";
-      #   content = {
-      #     type = "gpt";
-      #     partitions = {
-      #       ESP = {
-      #         size = "8G";
-      #         type = "EF00";
-      #         content = {
-      #           type = "filesystem";
-      #           format = "vfat";
-      #           mountpoint = "/boot";
-      #           mountOptions = [ "umask=0077" ];
-      #         };
-      #       };
-      #       zfs = {
-      #         size = "100%";
-      #         content = {
-      #           type = "zfs";
-      #           pool = "zroot";
-      #         };
-      #       };
-      #     };
-      #   };
-      # };
+      zroot1 = {
+        # Boot/Root SSD 1
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HBJQ-000_S50HNX0N611770";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "8G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
       # zroot2 = {
       #   # Boot/Root SSD 2
       #   type = "disk";
@@ -55,40 +55,40 @@
       #     };
       #   };
       # };
-      # blitz1 = {
-      #   # SSD 1
-      #   type = "disk";
-      #   device = "/dev/disk/by-id/ata-SAMSUNG_SSD_PM851_2.5_7mm_256GB_S1CUNSAG126805";
-      #   content = {
-      #     type = "gpt";
-      #     partitions = {
-      #       zfs = {
-      #         size = "100%";
-      #         content = {
-      #           type = "zfs";
-      #           pool = "blitz";
-      #         };
-      #       };
-      #     };
-      #   };
-      # };
-      # blitz2 = {
-      #   # SSD 2
-      #   type = "disk";
-      #   device = "/dev/disk/by-id/ata-SAMSUNG_SSD_PM851_2.5_7mm_256GB_S1CUNSAG125413";
-      #   content = {
-      #     type = "gpt";
-      #     partitions = {
-      #       zfs = {
-      #         size = "100%";
-      #         content = {
-      #           type = "zfs";
-      #           pool = "blitz";
-      #         };
-      #       };
-      #     };
-      #   };
-      # };
+      blitz1 = {
+        # SSD 1
+        type = "disk";
+        device = "/dev/disk/by-id/ata-SAMSUNG_SSD_PM851_2.5_7mm_256GB_S1CUNSAG126805";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "blitz";
+              };
+            };
+          };
+        };
+      };
+      blitz2 = {
+        # SSD 2
+        type = "disk";
+        device = "/dev/disk/by-id/ata-SAMSUNG_SSD_PM851_2.5_7mm_256GB_S1CUNSAG125413";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "blitz";
+              };
+            };
+          };
+        };
+      };
       tank1 = {
         # HDD 1
         type = "disk";
@@ -143,70 +143,70 @@
     };
 
     zpool = {
-      # zroot = {
-      #   type = "zpool";
-      #   # mode = "mirror";
+      zroot = {
+        type = "zpool";
+        # mode = "mirror";
 
-      #   mountpoint = "/";
-      #   rootFsOptions = {
-      #     "co.sun:auto-snapshot" = "false";
-      #   };
-      #   # postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
+        mountpoint = "/";
+        rootFsOptions = {
+          "co.sun:auto-snapshot" = "false";
+        };
+        # postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
 
-      #   datasets = {
+        datasets = {
 
-      #   };
-      # };
+        };
+      };
 
-      # blitz = {
-      #   type = "zpool";
-      #   mode = {
-      #     topology = {
-      #       type = "topology";
-      #       vdev = [
-      #         {
-      #           mode = "mirror";
-      #           members = [
-      #             "blitz1"
-      #             "blitz2"
-      #           ];
-      #         }
-      #       ];
-      #     };
-      #   };
+      blitz = {
+        type = "zpool";
+        mode = {
+          topology = {
+            type = "topology";
+            vdev = [
+              {
+                mode = "mirror";
+                members = [
+                  "blitz1"
+                  "blitz2"
+                ];
+              }
+            ];
+          };
+        };
 
-      #   rootFsOptions = {
-      #     mountpoint = "none";
-      #     "com.sun:auto-snapshot" = "false";
-      #   };
+        rootFsOptions = {
+          mountpoint = "none";
+          "com.sun:auto-snapshot" = "false";
+        };
 
-      #   datasets = {
-      #     nix = {
-      #       type = "zfs_fs";
-      #       mountpoint = "/nix";
-      #       options = {
-      #         compression = "zstd";
-      #         "com.sun:auto-snapshot" = "false";
-      #       };
-      #     };
-      #     var = {
-      #       type = "zfs_fs";
-      #       mountpoint = "/var";
-      #       options = {
-      #         compression = "zstd";
-      #         "com.sun:auto-snapshot" = "true";
-      #       };
-      #     };
-      #     home = {
-      #       type = "zfs_fs";
-      #       mountpoint = "/home";
-      #       options = {
-      #         compression = "zstd";
-      #         "com.sun:auto-snapshot" = "true";
-      #       };
-      #     };
-      #   };
-      # };
+        datasets = {
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options = {
+              compression = "zstd";
+              "com.sun:auto-snapshot" = "false";
+            };
+          };
+          var = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+            options = {
+              compression = "zstd";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+          home = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options = {
+              compression = "zstd";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+        };
+      };
 
       tank = {
         type = "zpool";
