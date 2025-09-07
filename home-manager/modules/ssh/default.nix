a84747edca4a5ec4e3d2bd387bba5c7b6e647ef8
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-09-07 15:40:23 +0200

    fix evaluation warning: home-manager ssh module

--- home-manager/modules/ssh/default.nix
+++ home-manager/modules/ssh/default.nix
@@ -14,7 +14,20 @@ in
   config = lib.mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false; # the "*" match block currently contains these default values
       matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
         "turing" = {
           hostname = "turing";
           user = "admin";
