a20ec7a47ea6a326559982e2b110149b53e41aa6
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-09-10 12:30:07 +0200

    remove old domains

--- modules/jellyfin/default.nix
+++ modules/jellyfin/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 let
@@ -112,47 +111,6 @@ in
             proxyWebsockets = true;
           };
         };
-
-        # TODO: remove (changed from proxy to 410 on 2025-05-06)
-        services.nginx.virtualHosts."${config.paul.private.domains.jellyfin_old}" =
-          let
-            web-root-dir = pkgs.writeTextFile {
-              name = "web-root";
-              text = ''
-                <!DOCTYPE html>
-                <html>
-                <head><meta charset="utf-8"/></head>
-                <body style="font-family: sans-serif; color: #ddd; background-color: #111">
-                  <div style="text-align: center; margin-top: 20%">
-                    <p style="font-size: 5em; margin: 0">🚧</p>
-                    <h1>Page has been moved</h1>
-                    <div sty>
-                    <p>The page you are looking for is now accessible at <a style="color: #a75ebd" href="https://${cfg.domain}">${cfg.domain}</a></p>
-                    <p>Please update your bookmarks and edit the domain in mobile and TV apps.</p>
-                    <p>If you need help setting up the apps you know who to call :)</p>
-                    <p style="font-size: 5em; margin: 0">🚧</p>
-                  </div>
-                </body>
-                </html>
-              '';
-              destination = "/errors/410.html";
-            };
-          in
-          {
-            enableACME = true;
-            forceSSL = true;
-            enableDyndns = cfg.enableDyndns;
-            root = "${web-root-dir}";
-            extraConfig = ''
-              error_page 410 /errors/410.html;
-            '';
-            locations."/" = {
-              return = "410";
-            };
-            locations."/errors/" = {
-              root = "${web-root-dir}";
-            };
-          };
       })
 
     ]
--- modules/jellyseerr/default.nix
+++ modules/jellyseerr/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 let
@@ -58,45 +57,6 @@ in
             geo-ip = true;
           };
         };
-
-        # TODO: remove (changed from 301 to 410 on 2025-05-06)
-        services.nginx.virtualHosts."${config.paul.private.domains.jellyseerr_old}" =
-          let
-            web-root-dir = pkgs.writeTextFile {
-              name = "web-root";
-              text = ''
-                <!DOCTYPE html>
-                <html>
-                <head><meta charset="utf-8"/></head>
-                <body style="font-family: sans-serif; color: #ddd; background-color: #111">
-                  <div style="text-align: center; margin-top: 20%">
-                    <p style="font-size: 5em; margin: 0">🚧</p>
-                    <h1>Page has been moved</h1>
-                    <p>The page you are looking for is now accessible at <a style="color: #a75ebd" href="https://${cfg.domain}">${cfg.domain}</a></p>
-                    <p>Please update your bookmarks.</p>
-                    <p style="font-size: 5em; margin: 0">🚧</p>
-                  </div>
-                </body>
-                </html>
-              '';
-              destination = "/errors/410.html";
-            };
-          in
-          {
-            enableACME = true;
-            forceSSL = true;
-            enableDyndns = cfg.enableDyndns;
-            root = "${web-root-dir}";
-            extraConfig = ''
-              error_page 410 /errors/410.html;
-            '';
-            locations."/" = {
-              return = "410";
-            };
-            locations."/errors/" = {
-              root = "${web-root-dir}";
-            };
-          };
       })
 
     ]
