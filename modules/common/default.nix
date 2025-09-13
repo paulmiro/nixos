4ca6a2fa8f928f5ffda42c43824d961c8d7e1c61
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-09-13 16:44:34 +0200

    allow some commands with passwordless sudo

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -28,5 +29,25 @@ in
 
     # to make sure clan vars can manage passwords correctly
     users.mutableUsers = false;
+
+    security.sudo.extraRules = [
+      {
+        commands = [
+          {
+            command = "/run/current-system/sw/bin/reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "/run/current-system/sw/bin/poweroff";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "/run/current-system/sw/bin/systemctl *";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+        groups = [ "wheel" ];
+      }
+    ];
   };
 }
