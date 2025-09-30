72fac7ded840fc15c4751ba5a57b7400e3e62707
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-09-30 23:44:20 +0200

    add turing ssh key

--- modules/user/root.nix
+++ modules/user/root.nix
@@ -17,10 +17,10 @@ in
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHjHjJ/vMoD3LId4IskuD9A6GwyTQXzt61IEq3/paul paulmiro@newton"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEom7NN1k9UCOTTiYsMBLqUK8BF8rjXTWAVQHSk/hwk paulmiro@hawking"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINirIGaLp4nLyJ4JUbCoknq5uqmp59uOfBrig0iW/mor paulmiro@morse"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINamsWVM0+zxXxUh5K1ww0GrdleakE3X8QSMJ+0b/btr paulmiro@better-laptop-paul-wsl"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKn+lCU7J7iNEhsCVK8chXmqeGsIAlbqhwbY8Rq/bll paulmiro@bell-avf"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESZxtxQOpcoxKjJO5f6XlVDmUSG7ZYz1fN5dIqi/tur paulmiro@turing"
       ];
     };
   };
