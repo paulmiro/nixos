e0cd81d67cafaebbac631a99a3f5594f04aeff8e
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-11-02 21:49:02 +0100

    switch to per-machine pipelines

--- pkgs/update-pipelines/default.nix
+++ pkgs/update-pipelines/default.nix
@@ -6,97 +6,166 @@
   ...
 }:
 let
+  # Map platform names between woodpecker and nix
   platforms = {
     "aarch64-linux" = "linux/arm64";
     "x86_64-linux" = "linux/amd64";
   };
-  forAllSystems = lib.genAttrs (builtins.attrNames platforms);
-  # Map platform names between woodpecker and nix
+
   nix = "nix --show-trace";
-  nixFlakeShow = {
-    name = "Nix flake show";
-    image = "bash";
-    commands = [ "${nix} flake show" ];
+
+  steps = {
+    nixFlakeShow = {
+      name = "Nix flake show";
+      image = "bash";
+      commands = [ "${nix} flake show" ];
+    };
+
+    nixFlakeCheck = {
+      name = "Nix flake check";
+      image = "bash";
+      commands = [ "${nix} flake check --show-trace" ];
+    };
+
+    decryptPrivateData = {
+      name = "Decrypt Private Data";
+      image = "bash";
+      commands = [
+        "cd modules/private"
+        "echo $AGE_KEY_PRIVATE_DATA | nix shell nixpkgs#age --command age --decrypt -i - -o private.toml.decrypted private.toml"
+        "mv private.toml.decrypted private.toml"
+      ];
+      environment.AGE_KEY_PRIVATE_DATA.from_secret = "AGE_KEY_PRIVATE_DATA";
+    };
+
+    atticSetup = {
+      name = "Setup Attic";
+      image = "bash";
+      commands = [
+        "attic login lounge-rocks https://cache.lounge.rocks $ATTIC_KEY --set-default"
+      ];
+      environment.ATTIC_KEY.from_secret = "attic_key";
+    };
   };
-  decryptPrivateDataStep = {
-    name = "Decrypt Private Data";
-    image = "bash";
-    commands = [
-      "cd modules/private"
-      "echo $AGE_KEY_PRIVATE_DATA | nix shell nixpkgs#age --command age --decrypt -i - -o private.toml.decrypted private.toml"
-      "mv private.toml.decrypted private.toml"
-    ];
-    environment.AGE_KEY_PRIVATE_DATA.from_secret = "AGE_KEY_PRIVATE_DATA";
+
+  when = {
+    branch = {
+      include = [ "**" ];
+      exclude = [ "no-private-data" ];
+    };
+    event = [ "push" ];
   };
-  nixFlakeCheck = {
-    name = "Nix flake check";
-    image = "bash";
-    commands = [ "${nix} flake check --show-trace" ];
-  };
-  atticSetupStep = {
-    name = "Setup Attic";
-    image = "bash";
-    commands = [
-      "attic login lounge-rocks https://cache.lounge.rocks $ATTIC_KEY --set-default"
-    ];
-    environment.ATTIC_KEY.from_secret = "attic_key";
-  };
-  pipelines = forAllSystems (
-    system:
-    pkgs.writeText "pipeline.json" (
-      builtins.toJSON {
-        labels = {
-          backend = "local";
-          platform = platforms."${system}";
-        };
-        when = {
-          branch = {
-            include = [ "**" ];
-            exclude = [ "no-private-data" ];
-          };
-          event = [ "push" ];
-        };
-        steps = pkgs.lib.lists.flatten (
-          [
-            decryptPrivateDataStep
-            nixFlakeShow
+
+  nixosConfigurations = lib.filterAttrs (
+    name: config: config.config.paul.ci.enable
+  ) flake-self.nixosConfigurations;
+
+  systemFor = config: config.config.nixpkgs.hostPlatform.system;
+
+  toFile = pipeline: pkgs.writeText "pipeline.json" (builtins.toJSON pipeline);
+
+  mkMachinePipeline =
+    {
+      name,
+      config,
+      dependsOn,
+    }:
+    {
+      labels = {
+        backend = "local";
+        platform = platforms."${systemFor config}";
+      };
+      inherit when;
+      depends_on = dependsOn;
+      steps = [
+        steps.decryptPrivateData
+        steps.atticSetup
+        {
+          name = "Build ${name}";
+          image = "bash";
+          commands = [
+            "${nix} build --print-out-paths '.#nixosConfigurations.${name}.config.system.build.toplevel' -o 'result-${name}'"
+          ];
+        }
+        {
+          name = "Show ${name} info";
+          image = "bash";
+          commands = [
+            "${nix} path-info --closure-size -h $(readlink -f 'result-${name}')"
+          ];
+        }
+        {
+          name = "Push ${name} to Attic";
+          image = "bash";
+          commands = [ "attic push lounge-rocks:nix-cache 'result-${name}'" ];
+        }
+      ];
+    };
+
+  machinePipelines = builtins.listToAttrs (
+    lib.foldlAttrs
+      (
+        acc: name: value:
+        (
+          let
+            prev = lib.lists.last acc;
+            system = systemFor value;
+          in
+          (if prev.initial then [ ] else acc)
+          ++ [
+            {
+              initial = false;
+              prevNames = prev.prevNames // {
+                "${system}" = "build-${name}";
+              };
+              name = "build-${name}";
+              value = mkMachinePipeline {
+                config = value;
+                inherit name;
+                # we depend on the previous machine's pipeline to make sure we don't build shared packages twice
+                dependsOn = [ prev.prevNames."${system}" ];
+              };
+            }
           ]
-          ++ lib.optionals ("${system}" == "x86_64-linux") [ nixFlakeCheck ]
-          ++ [ atticSetupStep ]
-          ++ (map
-            (host: [
-              {
-                name = "Build ${host}";
-                image = "bash";
-                commands = [
-                  "${nix} build --print-out-paths '.#nixosConfigurations.${host}.config.system.build.toplevel' -o 'result-${host}'"
-                ];
-              }
-              {
-                name = "Show ${host} info";
-                image = "bash";
-                commands = [
-                  "${nix} path-info --closure-size -h $(readlink -f 'result-${host}')"
-                ];
-              }
-              {
-                name = "Push ${host} to Attic";
-                image = "bash";
-                commands = [ "attic push lounge-rocks:nix-cache 'result-${host}'" ];
-              }
-            ])
-            (
-              builtins.attrNames (
-                lib.filterAttrs (
-                  name: config: (config.config.nixpkgs.hostPlatform.system == system && config.config.paul.ci.enable)
-                ) flake-self.nixosConfigurations
-              )
-            )
-          )
-        );
-      }
-    )
+        )
+      )
+      # initial accumulator
+      [
+        {
+          initial = true;
+          # the first pipeline on each system should depend on the nix-flake-check step
+          prevNames = {
+            "aarch64-linux" = "nix-flake-check";
+            "x86_64-linux" = "nix-flake-check";
+          };
+        }
+      ]
+      nixosConfigurations
   );
+
+  pipelines = machinePipelines // {
+    nix-flake-show = {
+      labels = {
+        backend = "local";
+        platform = "linux/amd64";
+      };
+      inherit when;
+      steps = [
+        steps.nixFlakeShow
+      ];
+    };
+    nix-flake-check = {
+      labels = {
+        backend = "local";
+        platform = "linux/amd64";
+      };
+      inherit when;
+      depends_on = [ "nix-flake-show" ];
+      steps = [
+        steps.nixFlakeCheck
+      ];
+    };
+  };
 in
 pkgs.writeShellScriptBin "woodpecker-pipeline" ''
   # make sure .woodpecker folder exists
@@ -106,6 +175,9 @@ pkgs.writeShellScriptBin "woodpecker-pipeline" ''
   rm -rf .woodpecker/*
     
   # copy pipelines to .woodpecker folder
-  cat ${pipelines.aarch64-linux} | ${pkgs.yq}/bin/yq -y -w 9999 > .woodpecker/arm64-linux.yaml
-  cat ${pipelines.x86_64-linux} | ${pkgs.yq}/bin/yq -y -w 9999 > .woodpecker/x86-linux.yaml
+  ${lib.concatStringsSep "\n" (
+    lib.mapAttrsToList (name: pipeline: ''
+      cat ${toFile pipeline} | ${pkgs.yq}/bin/yq -y -w 9999 > .woodpecker/${name}.yaml
+    '') pipelines
+  )}
 ''
