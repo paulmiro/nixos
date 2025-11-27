662024f5812af37e2c2ea9cc87c926b5d1d27fee
paulmiro <30203227+paulmiro@users.noreply.github.com>
2025-11-27 20:37:17 +0100

    split nvidia module into generic and laptop

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -23,64 +23,61 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    # Load nvidia driver for Xorg and Wayland
-    services.xserver.videoDrivers = [ "nvidia" ];
+  config = lib.mkIf cfg.enable lib.mkMerge [
+    {
+      # Load nvidia driver for Xorg and Wayland
+      services.xserver.videoDrivers = [ "nvidia" ];
 
-    environment.systemPackages = with pkgs; [
-      # nvtopPackages.full # TODO: broken (also: switch to .nvidia?)
-    ];
+      environment.systemPackages = with pkgs; [
+        # nvtopPackages.full # TODO: broken (also: switch to .nvidia?)
+      ];
 
-    hardware = {
-      # Enable OpenGL
-      graphics = {
-        enable = true;
-        enable32Bit = true;
-        extraPackages = lib.mkIf cfg.laptop (
-          with pkgs;
-          [
-            libva-vdpau-driver
-          ]
-        );
-      };
-
-      nvidia = {
-        # Modesetting is required.
-        modesetting.enable = lib.mkIf cfg.laptop true;
-
-        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-        powerManagement.enable = lib.mkIf cfg.laptop false;
-
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-        powerManagement.finegrained = false;
-
-        # Use the NVidia open source kernel module (not to be confused with the
-        # independent third-party "nouveau" open source driver).
-        # Support is limited to the Turing and later architectures. Full list of
-        # supported GPUs is at:
-        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-        # Only available from driver 515.43.04+
-        # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-        open = lib.mkIf cfg.laptop false;
-
-        # Enable the Nvidia settings menu,
-        # accessible via `nvidia-settings`.
-        nvidiaSettings = true;
-
-        # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-        prime = lib.mkIf cfg.laptop {
-          intelBusId = cfg.intelBusId;
-          nvidiaBusId = cfg.nvidiaBusId;
-          offload.enable = true;
+      hardware = {
+        # Enable OpenGL
+        graphics = {
+          enable = true;
+          enable32Bit = true;
         };
+
+        nvidia = {
+          # Fine-grained power management. Turns off GPU when not in use.
+          # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+          powerManagement.finegrained = false;
+          # Enable the Nvidia settings menu,
+          # accessible via `nvidia-settings`.
+          nvidiaSettings = true;
+          # Optionally, you may need to select the appropriate driver version for your specific GPU.
+          package = config.boot.kernelPackages.nvidiaPackages.beta;
+        };
+        nvidia-container-toolkit.enable = lib.mkIf config.virtualisation.docker.enable true;
       };
+    }
 
-      nvidia-container-toolkit.enable = lib.mkIf config.virtualisation.docker.enable true;
+    (lib.mkIf cfg.laptop {
+      extraPackages = with pkgs; [
+        libva-vdpau-driver
+      ];
 
-    };
+      # Modesetting is required for the unfree driver.
+      modesetting.enable = true;
 
-  };
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      powerManagement.enable = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Do not disable this unless your GPU is unsupported or if you have a good reason to.
+      open = false;
+
+      prime = {
+        intelBusId = cfg.intelBusId;
+        nvidiaBusId = cfg.nvidiaBusId;
+        offload.enable = true;
+      };
+    })
+  ];
 }
