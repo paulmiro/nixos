b4c705e17dcbd5007a3508db220461cb111610dc
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-01-31 22:58:03 +0100

    edit meilisearch module: remove options that are now the default

--- modules/meilisearch/default.nix
+++ modules/meilisearch/default.nix
@@ -15,12 +15,6 @@ in
   config = lib.mkIf cfg.enable {
     services.meilisearch = {
       enable = true;
-      # this is required because state-version < 25.05 sets the package to 1.11
-      # TODO: 25.11: remove this (assuming they actually removed meilisearch_1_11)
-      # -> also check if the dumpless upgrade is made the default, and remove that too if that's the case
-      package = pkgs.meilisearch;
-      # this allows the database to be updated automatically without having to dump and then re-import it
-      settings.experimental_dumpless_upgrade = true;
     };
 
     clan.core.state.meilisearch = {
