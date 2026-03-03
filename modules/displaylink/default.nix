b02e8ae394556fa787aa01d6801d4164b50d6155
Paul Mika Rohde <git@paulmiro.de>
2026-03-03 11:52:59 +0100

    move displaylink to new module and reenable

---
+++ modules/displaylink/default.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.paul.displaylink;
+in
+{
+  options.paul.displaylink = {
+    enable = lib.mkEnableOption "enable displaylink support";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # enable the driver
+    services.xserver.videoDrivers = [ "displaylink" ];
+    # auto-start the service
+    systemd.services.dlm.wantedBy = [ "multi-user.target" ];
+
+    # TODO do we need this?
+    # environment.systemPackages = with pkgs; [ displaylink ];
+
+    # downloading this means agreeing to their EULA
+    nixpkgs.overlays = [
+      (self: super: {
+        displaylink = super.displaylink.overrideAttrs {
+          src = pkgs.fetchurl {
+            name = "displaylink-620.zip";
+            url = "https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.2-EXE.zip";
+            hash = "sha256-JQO7eEz4pdoPkhcn9tIuy5R4KyfsCniuw6eXw/rLaYE=";
+          };
+        };
+      })
+    ];
+
+  };
+}
