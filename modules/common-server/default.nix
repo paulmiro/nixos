35cba498fde028132d917160a059624d86fea731
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-03-15 20:10:12 +0100

    remove obsolete todo

--- modules/common-server/default.nix
+++ modules/common-server/default.nix
@@ -28,20 +28,6 @@ in
     systemd = {
       enableEmergencyMode = false;
 
-      # TODO: disabled for now because turing is weird
-      # watchdog = {
-      #   # systemd will send a signal to the hardware watchdog at half
-      #   # the interval defined here, so every 10s.
-      #   # If the hardware watchdog does not get a signal for 20s,
-      #   # it will forcefully reboot the system.
-      #   runtimeTime = "20s";
-      #   # Forcefully reboot if the final stage of the reboot
-      #   # hangs without progress for more than 30s.
-      #   # For more info, see:
-      #   #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-      #   rebootTime = "30s";
-      # };
-
       sleep.settings.Sleep = {
         AllowSuspend = "no";
         AllowHibernation = "no";
