fb179183605e5b01d4d419b80a539bf81d32f0eb
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-03-15 15:11:28 +0100

    add windows mounts for hawking

--- machines/hawking/configuration.nix
+++ machines/hawking/configuration.nix
@@ -22,4 +22,19 @@
   };
 
   clan.core.deployment.requireExplicitUpdate = true;
+
+  fileSystems."/mnt/windows/C" = {
+    device = "/dev/disk/by-id/wwn-0x5002538d422793ef-part4";
+    fsType = "ntfs";
+  };
+
+  fileSystems."/mnt/windows/D" = {
+    device = "/dev/disk/by-id/wwn-0x50014ee20ee3e95b-part1";
+    fsType = "ntfs";
+  };
+
+  fileSystems."/mnt/windows/G" = {
+    device = "/dev/disk/by-id/wwn-0x500a0751e4b67f74-part2";
+    fsType = "ntfs";
+  };
 }
