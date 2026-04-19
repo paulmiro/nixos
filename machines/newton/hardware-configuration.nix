ab467f5e3f1811961dd9aa71b36a1879b51045ed
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-04-19 19:41:51 +0200

    newton: add swapfile, enable zswap

--- machines/newton/hardware-configuration.nix
+++ machines/newton/hardware-configuration.nix
@@ -44,7 +44,14 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 32 * 1024;
+    }
+  ];
+
+  boot.zswap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
