c5d18425b75ba4bee66fb5dd70cd976adecc0f81
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-04-19 19:42:55 +0200

    leibniz: add swapfile, enable zswap

--- machines/leibniz/hardware-configuration.nix
+++ machines/leibniz/hardware-configuration.nix
@@ -46,7 +46,14 @@
     fsType = "ntfs";
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 32 * 1024;
+    }
+  ];
+
+  boot.zswap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
