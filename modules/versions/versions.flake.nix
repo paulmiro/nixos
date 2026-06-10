95f3efa9e2fbe5393ffeacf6a5dd005313704610
Paul Mika Rohde <git@paulmiro.de>
2026-06-10 02:51:24 +0200

    move versions updater package definition to module

--- modules/versions/versions.flake.nix
+++ modules/versions/versions.flake.nix
@@ -3,4 +3,15 @@
   flake.versions = lib.mapAttrs (name: value: value.version) (
     fromTOML (builtins.readFile ./versions.toml)
   );
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.update-versions = pkgs.writers.writePython3Bin "update-versions" {
+        libraries = [ pkgs.python3Packages.pygithub ];
+        flakeIgnore = [
+          "E501"
+        ];
+      } (builtins.readFile ./main.py);
+    };
 }
