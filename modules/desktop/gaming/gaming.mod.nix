7ac368525f2933db52b53f8548cc1dad2a8a8ace
Paul Mika Rohde <git@paulmiro.de>
2026-06-24 22:42:05 +0200

    remove openldsp test fix

--- modules/desktop/gaming/gaming.mod.nix
+++ modules/desktop/gaming/gaming.mod.nix
@@ -13,16 +13,6 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-
-    # TODO fix for nixpkgs#513245
-    nixpkgs.overlays = [
-      (_: prev: {
-        openldap = prev.openldap.overrideAttrs {
-          doCheck = !prev.stdenv.hostPlatform.isi686;
-        };
-      })
-    ];
-
     programs.steam = {
       enable = true;
       extraCompatPackages = [ (pkgs.proton-ge-bin.override { steamDisplayName = "GE-Proton-Nix"; }) ];
