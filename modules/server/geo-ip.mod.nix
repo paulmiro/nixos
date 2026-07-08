106c5d1baf814c85d8dc4ff5de486691c1aefc5d
Paul Mika Rohde <git@paulmiro.de>
2026-07-08 16:34:27 +0200

    add per-vhost and per-location geo-ip allow lists

--- modules/server/geo-ip.mod.nix
+++ modules/server/geo-ip.mod.nix
@@ -16,6 +16,33 @@ let
     (!cfg.forceDisable)
     # auto-enable when at least one vhost or location has the option set
     && ((builtins.length enabledVhosts) != 0 || (builtins.length enabledLocations) != 0);
+
+  mkAclName = countries: lib.toLower (lib.concatStringsSep "_" countries);
+
+  allCountryLists = lib.unique (
+    (map (vhost: vhost.value.geoIpCountries) enabledVhosts)
+    ++ (map (loc: loc.value.geoIpCountries) enabledLocations)
+  );
+
+  acls = map (countries: {
+    name = mkAclName countries;
+    countries = countries;
+  }) allCountryLists;
+
+  defaultCountries = [
+    "AT"
+    "AU"
+    "DE"
+    "DK"
+    "ES"
+    "FR"
+    "GB"
+    "IT"
+    "JP"
+    "LU"
+    "NL"
+    "US"
+  ];
 in
 {
   options.paul.geo-ip = {
@@ -29,13 +56,40 @@ in
         {
           options = {
             enableGeoIP = lib.mkEnableOption "enable geo-ip for this vhost";
+            geoIpCountries = lib.mkOption {
+              description = "List of countries to allow requests from";
+              type = lib.types.listOf lib.types.str;
+              default = defaultCountries;
+              apply = it: lib.naturalSort (lib.unique it);
+            };
           };
-          config.extraConfig = toString [
-            (lib.optional (config.enableGeoIP && !cfg.forceDisable) ''
-              if ($allowed_country = no) {
-                return 444;
-              }
-            '')
+
+          config = lib.mkMerge [
+            # we cannot use `lib.mkIf enable` here because of infinite recursion
+            (lib.mkIf (!cfg.forceDisable) {
+              # this is added to all vhosts, regardless of enableGeoIP,
+              # because we cannot determine if a location has it set
+              extraConfig = ''
+                error_page 452 =403 @geoblocked;
+              '';
+              locations."@geoblocked" = {
+                root = ./.;
+                tryFiles = "/geoblocked.html =404";
+                extraConfig = ''
+                  internal;
+                  default_type text/html;
+                '';
+              };
+            })
+            (lib.mkIf (config.enableGeoIP && !cfg.forceDisable) {
+              extraConfig = ''
+                set $geo_acl ${mkAclName config.geoIpCountries};
+
+                if ($geo_blocked) {
+                  return 452;
+                }
+              '';
+            })
           ];
 
           options.locations = lib.mkOption {
@@ -45,14 +99,23 @@ in
                 {
                   options = {
                     enableGeoIP = lib.mkEnableOption "enable geo-ip";
+                    geoIpCountries = lib.mkOption {
+                      description = "List of countries to allow requests from";
+                      type = lib.types.listOf lib.types.str;
+                      default = defaultCountries;
+                      apply = it: lib.naturalSort (lib.unique it);
+                    };
                   };
-                  config.extraConfig = toString [
-                    (lib.optional (config.enableGeoIP && !cfg.forceDisable) ''
-                      if ($allowed_country = no) {
-                        return 444;
+
+                  config = lib.mkIf (config.enableGeoIP && !cfg.forceDisable) {
+                    extraConfig = ''
+                      set $geo_acl ${mkAclName config.geoIpCountries};
+
+                      if ($geo_blocked) {
+                        return 452;
                       }
-                    '')
-                  ];
+                    '';
+                  };
                 }
               )
             );
@@ -62,64 +125,60 @@ in
     );
   };
 
-  config = lib.mkIf enable {
-
-    services.geoipupdate = {
-      enable = true;
-      interval = "weekly";
-      settings = {
-        EditionIDs = [ "GeoLite2-Country" ];
-        AccountID = 767585;
-        LicenseKey = config.clan.core.vars.generators.maxmind-license-key.files.key.path;
+  config = lib.mkMerge [
+    (lib.mkIf enable {
+      services.geoipupdate = {
+        enable = true;
+        interval = "weekly";
+        settings = {
+          EditionIDs = [ "GeoLite2-Country" ];
+          AccountID = 767585;
+          LicenseKey = config.clan.core.vars.generators.maxmind-license-key.files.key.path;
+        };
       };
-    };
 
-    clan.core.vars.generators.maxmind-license-key = {
-      prompts.key.description = "MaxMind License Key (see bw)";
-      prompts.key.type = "hidden";
-      prompts.key.persist = false;
+      clan.core.vars.generators.maxmind-license-key = {
+        prompts.key.description = "MaxMind License Key (see bw)";
+        prompts.key.type = "hidden";
+        prompts.key.persist = false;
 
-      files.key.secret = true;
+        files.key.secret = true;
 
-      share = true;
+        share = true;
 
-      script = "cp $prompts/key $out/key";
-    };
+        script = "cp $prompts/key $out/key";
+      };
 
-    # build nginx with geoip2 module
-    services.nginx = {
-      package = pkgs.nginxStable.override (oldAttrs: {
-        modules = with pkgs.nginxModules; [ geoip2 ];
-        buildInputs = oldAttrs.buildInputs ++ [ pkgs.libmaxminddb ];
-      });
-      appendHttpConfig = toString [
-        # we want to load the geoip2 module in our http config, pointing to the database we are using
-        # the country iso code is the only data we need
-        ''
+      # build nginx with geoip2 module
+      services.nginx = {
+        additionalModules = with pkgs.nginxModules; [ geoip2 ];
+        appendHttpConfig = ''
           geoip2 ${config.services.geoipupdate.settings.DatabaseDirectory}/GeoLite2-Country.mmdb {
+            auto_reload 60m;
             $geoip2_data_country_iso_code country iso_code;
           }
-        ''
-        # we want to allow only requests from specific countries
-        # if a request is not from such a country, we return no, which will result in a 403
-        ''
-          map $geoip2_data_country_iso_code $allowed_country {
-            default no;
-            DE yes;
-            AT yes; # for some reason morse gets identified as austria
-            ES yes;
-            FR yes;
-            GB yes;
-            IT yes;
-            NL yes;
-            DK yes;
-            JP yes;
-            LU yes;
-            US yes;
-            AU yes;
+
+          map "$geo_acl:$geoip2_data_country_iso_code" $geo_blocked {
+            default 1;
+
+            # Disable GeoIP blocking for this location/profile
+            ~^off: 0;
+
+          ${lib.concatStringsSep "\n" (
+            map (acl: ''
+                # Profile: ${acl.name}
+              ${lib.concatStringsSep "  \n" (map (country: "  ${acl.name}:${country} 0;") acl.countries)}
+            '') acls
+          )}
           }
-        ''
-      ];
-    };
-  };
+        '';
+      };
+    })
+
+    (lib.mkIf cfg.forceDisable {
+      warnings = ''
+        `paul.geo-ip.forceDisable` is only meant to temporarily disable the module.
+      '';
+    })
+  ];
 }
