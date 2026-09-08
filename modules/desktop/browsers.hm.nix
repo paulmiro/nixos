77bbd753c5460958cf7e3d0c731084fbdb16729d
Paul Mika Rohde <git@paulmiro.de>
2026-09-08 16:38:07 +0200

    remove firefox override

--- modules/desktop/browsers.hm.nix
+++ modules/desktop/browsers.hm.nix
@@ -27,7 +27,6 @@ in
       programs = {
         firefox = {
           enable = true;
-          package = pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) { };
           configPath = "${config.xdg.configHome}/mozilla/firefox";
         };
       };
